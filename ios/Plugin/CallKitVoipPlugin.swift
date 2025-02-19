6ec0236f5ee3f5325762766f06ee7ea6285eaaf1
Erik Ramalho <5274068+erikramalho@users.noreply.github.com>
2025-02-19 16:34:38 -0300

    Update CallKitVoipPlugin.swift

--- ios/Plugin/CallKitVoipPlugin.swift
+++ ios/Plugin/CallKitVoipPlugin.swift
@@ -3,7 +3,6 @@ import Capacitor
 import UIKit
 import CallKit
 import PushKit
-import WebRTCiOSSDK
 
 /**
  *  CallKit Voip Plugin provides native PushKit functionality with apple CallKit to capacitor
